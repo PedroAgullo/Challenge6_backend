fe99e1b47927fcb15301c5c8bebe4e0da9c25979
pedroAGullo <pedro.agullo.marco@gmail.com>
2021-06-02 23:39:24 +0200

    Added max number of users in a room.

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -19,9 +19,29 @@ class Sala {
     async joinRoom(data){
         const id = data.id;
         const member = data.member;
+
+        //Aqui empieza la fiesta...
+        let arrayRoom = [];  //Declaramos el array vacio.
+
+        //Encontramos la room mediante el id. Nos devuelve la coleccion completa.
+        let room = await Room.findOne(
+            {_id: id}
+        )
+
+
+        arrayRoom = room.members;  //Metemos en arrayRoom el array completo de usuarios de la actividad.
+        let roomStatus = arrayRoom.length; //roomStatus coge el número de users que hay en el array.
+
+        //Comparamos roomStatus, si es mayor que 5 nos dice que la sala está llena, sino pasa a añadir el user.
+        if (roomStatus > 5){
+            throw new Error ( "La clase está llena.");  
+        }
+
+        //Esta parte nos añade el usuario a la clase si hay sitio disponible.
         return Room.findByIdAndUpdate(
             {_id: id},
             {$push: {members: member}});
+        
     }
 
     async joinRoomCoach(data){
